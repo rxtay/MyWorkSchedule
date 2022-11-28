185a191d2a5bc9dfa5ca5ecf10d005b05026971a
miyokoysy <2105039H@tp.edu.sg>
2022-11-28 16:37:24 +0800

    Updated BranchDao.java to include list of employee branches

--- src/main/java/com/example/myworkschedule/dao/Branch/BranchDao.java
+++ src/main/java/com/example/myworkschedule/dao/Branch/BranchDao.java
@@ -1,7 +1,11 @@
 package com.example.myworkschedule.dao.Branch;
 
+import com.example.myworkschedule.beans.Branch;
 import com.example.myworkschedule.beans.BranchShift;
+
+import java.util.List;
 
 public interface BranchDao {
     BranchShift getBranch(int branchId);
+    List<Branch> getEmployeeBranches(int employeeId);
 }
