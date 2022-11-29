5ae5ea3cd291773a474196c1a3036ae3660a1ff8
rxtay <77395408+rxtay@users.noreply.github.com>
2022-11-29 12:58:38 +0800

    Implemented doPost method in LoginServlet.java to handle login.

--- src/main/java/com/example/myworkschedule/servlets/LoginServlet.java
+++ src/main/java/com/example/myworkschedule/servlets/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.example.myworkschedule.servlets;
 
+import com.example.myworkschedule.beans.User;
+import com.example.myworkschedule.dao.User.UserDaoImpl;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -14,6 +17,15 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        UserDaoImpl dao = new UserDaoImpl();
+        User user = dao.login(email, password);
+        if (user == null) {
+            request.setAttribute("alert", "Either email or password is incorrect. Please try again.");
+        } else {
+            request.setAttribute("alert", String.format("Welcome to MyWorkSchedule, %s.", user.getEmail()));
+        }
+        doGet(request, response);
     }
 }
