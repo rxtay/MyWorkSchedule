8ad20d5a8d98ff8ac03e67728b1e1e1810894267
rxtay <77395408+rxtay@users.noreply.github.com>
2022-11-30 20:11:55 +0800

    Included insert branch method to BranchDao interface.

--- src/main/java/com/example/myworkschedule/dao/Branch/BranchDao.java
+++ src/main/java/com/example/myworkschedule/dao/Branch/BranchDao.java
@@ -8,4 +8,5 @@ import java.util.List;
 public interface BranchDao {
     BranchShift getBranch(int branchId);
     List<Branch> getEmployeeBranches(int employeeId);
+    void insertBranch(Branch branch);
 }
