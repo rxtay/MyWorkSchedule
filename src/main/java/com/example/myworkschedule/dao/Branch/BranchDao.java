75be1d8d2e72c16245b619fcf918a334855a4888
rxtay <77395408+rxtay@users.noreply.github.com>
2022-11-30 22:52:27 +0800

    Change return type of insertBranch method from void to DataOrException<Boolean>.

--- src/main/java/com/example/myworkschedule/dao/Branch/BranchDao.java
+++ src/main/java/com/example/myworkschedule/dao/Branch/BranchDao.java
@@ -2,11 +2,12 @@ package com.example.myworkschedule.dao.Branch;
 
 import com.example.myworkschedule.beans.Branch;
 import com.example.myworkschedule.beans.BranchShift;
+import com.example.myworkschedule.beans.DataOrException;
 
 import java.util.List;
 
 public interface BranchDao {
     BranchShift getBranch(int branchId);
     List<Branch> getEmployeeBranches(int employeeId);
-    void insertBranch(Branch branch);
+    DataOrException<Boolean> insertBranch(Branch branch);
 }
