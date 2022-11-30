842c338310514a4256a42ebb36c2ec38e9061306
rxtay <77395408+rxtay@users.noreply.github.com>
2022-11-30 23:03:25 +0800

    Added doPost method in EmployerBranchesServlet.java to handle form (add branch) submit event.

--- src/main/java/com/example/myworkschedule/servlets/EmployerBranchesServlet.java
+++ src/main/java/com/example/myworkschedule/servlets/EmployerBranchesServlet.java
@@ -1,16 +1,15 @@
 package com.example.myworkschedule.servlets;
 
 import com.example.myworkschedule.beans.Branch;
+import com.example.myworkschedule.beans.DataOrException;
 import com.example.myworkschedule.dao.Branch.BranchDaoImpl;
-
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.util.List;
 
 @WebServlet(name = "EmployerBranchesServlet", value = "/EmployerBranchesServlet")
@@ -21,5 +20,23 @@ public class EmployerBranchesServlet extends HttpServlet {
         List<Branch> branches = dao.showEmployerBranches(1);
         request.setAttribute("branches", branches);
         request.getRequestDispatcher("/views/EmployerBranches.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doPost: EmployerBranchesServlet");
+        int employerId = 1;
+        String name = req.getParameter("name");
+        BranchDaoImpl dao = new BranchDaoImpl();
+        Branch branch = new Branch();
+        branch.setEmployerId(employerId);
+        branch.setName(name);
+        DataOrException<Boolean> result = dao.insertBranch(branch);
+        if (result.e != null) {
+            req.setAttribute("message", "Failed to add branch.");
+        } else {
+            req.setAttribute("message", "Branch added successfully.");
+        }
+        doGet(req, resp);
     }
 }
