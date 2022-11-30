0a6c6511360a08ac08a12a7497b4e9513252d02a
rxtay <77395408+rxtay@users.noreply.github.com>
2022-11-30 14:18:35 +0800

    Resolved local branch merged with origin master.

--- src/main/java/com/example/myworkschedule/dao/User/UserDaoImpl.java
+++ src/main/java/com/example/myworkschedule/dao/User/UserDaoImpl.java
@@ -2,13 +2,89 @@ package com.example.myworkschedule.dao.User;
 
 import com.example.myworkschedule.beans.User;
 import com.example.myworkschedule.dao.DatabaseConnection;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class UserDaoImpl implements UserDao {
+    @Override
+    public int insertNewEmployeeId(){
+        int employeeId;
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            String query = "INSERT INTO employee values ()";
+            PreparedStatement statement = connection.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+            statement.executeUpdate();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()){
+                if (generatedKeys.next()){
+                    employeeId = generatedKeys.getInt(1);
+                }else {
+                    throw new SQLException("Creating new employeeId failed.");
+                }
+            }
+        } catch (SQLException e){
+            throw  new RuntimeException(e);
+        }
+        return employeeId;
+    }
+    @Override
+    public int insertNewEmployerId(){
+        int employerId;
+        try {
+            Connection connection = DatabaseConnection.getConnection();
+            String query = "INSERT INTO employer values ()";
+            PreparedStatement statement = connection.prepareStatement(query,Statement.RETURN_GENERATED_KEYS);
+            statement.executeUpdate();
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()){
+                if (generatedKeys.next()){
+                    employerId = generatedKeys.getInt(1);
+                }else {
+                    throw new SQLException("Creating new employerId failed.");
+                }
+            }
+        } catch (SQLException e){
+            throw  new RuntimeException(e);
+        }
+        return employerId;
+    }
+    @Override
+    public int registerUser(User user, String role){
+
+        Connection connection = DatabaseConnection.getConnection();
+        int rowsaffected;
+        int employeeId;
+        int employerId;
+        System.out.println(role);
+        try{
+            if(role.equals("employee")){
+                employeeId = insertNewEmployeeId();
+                String query = "INSERT INTO user (firstName, lastName, email,password, employeeId)values (?,?,?,?,?)";
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setString(1,user.getFirstName());
+                statement.setString(2,user.getLastName());
+                statement.setString(3, user.getEmail());
+                statement.setString(4, user.getPassword());
+                statement.setInt(5,employeeId);
+
+                rowsaffected = statement.executeUpdate();
+            }else if (role.equals("employer")){
+                employerId = insertNewEmployerId();
+
+                String query = "INSERT INTO user (firstName, lastName, email,password ,employer_employerId)values (?,?,?,?,?)";
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setString(1,user.getFirstName());
+                statement.setString(2,user.getLastName());
+                statement.setString(3, user.getEmail());
+                statement.setString(4, user.getPassword());
+                statement.setInt(5,employerId);
+
+                rowsaffected = statement.executeUpdate();
+            }else{
+                rowsaffected = 0;
+            }
+        }catch(SQLException e) {
+            throw  new RuntimeException(e);
+        }
+        return rowsaffected;
+    }
     @Override
     public User login(String email, String password) {
         try {
@@ -34,8 +110,7 @@ public class UserDaoImpl implements UserDao {
             String lastName = set.getString("lastName");
             Integer employeeId = set.getInt("employeeId");
             Integer employerId = set.getInt("employer_employerId");
-            User user = new User(userId, firstName, lastName, email, employeeId, employerId);
-            return user;
+            return new User(userId, firstName, lastName, email, password, employeeId, employerId);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
