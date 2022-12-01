a6a8b6a881cad0a76f6eb45b09b0310827460e5c
miyokoysy <2105039H@tp.edu.sg>
2022-12-01 14:19:09 +0800

    Created a ViewBranchEmployeesShiftServlet.java to retrieve branch’s employee’s shift

---
+++ src/main/java/com/example/myworkschedule/servlets/ViewBranchEmployeesShiftServlet.java
@@ -0,0 +1,28 @@
+package com.example.myworkschedule.servlets;
+
+import com.example.myworkschedule.beans.Shift;
+import com.example.myworkschedule.dao.Branch.BranchDaoImpl;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "ViewBranchEmployeesShiftServlet", value = "/branch/employee/shift")
+public class ViewBranchEmployeesShiftServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        BranchDaoImpl dao = new BranchDaoImpl();
+        List<Shift> shift = dao.getEmployeeShift(111);
+        System.out.println(shift);
+
+        request.setAttribute("shifts", shift);
+        request.getRequestDispatcher("/views/ViewEmployeesShift.jsp").forward(request, response);
+    };
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
