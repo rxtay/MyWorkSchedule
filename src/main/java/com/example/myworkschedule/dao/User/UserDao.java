9bce78e33a55cb6ae5c85b90517ba38f5fd2b258
rxtay <77395408+rxtay@users.noreply.github.com>
2022-12-02 21:27:56 +0800

    Added searchBranchEmployee method to UserDao interface.

--- src/main/java/com/example/myworkschedule/dao/User/UserDao.java
+++ src/main/java/com/example/myworkschedule/dao/User/UserDao.java
@@ -1,12 +1,12 @@
-package com.example.myworkschedule.dao.user;
+package com.example.myworkschedule.dao.User;
 
 import com.example.myworkschedule.beans.User;
 
+import java.util.List;
+
 public interface UserDao {
-
     int insertNewEmployeeId();
-
     int insertNewEmployerId();
-
     int registerUser(User user, String role);
+    List<User> searchBranchEmployee(int branchId);
 }
