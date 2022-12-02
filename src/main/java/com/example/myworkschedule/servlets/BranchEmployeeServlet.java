5113a62b943bd695154f63aec7344c487443810d
rxtay <77395408+rxtay@users.noreply.github.com>
2022-12-02 21:35:23 +0800

    Changed import for UserDao from UserDao to UserDaoImpl in BranchEmployeeServlet.java.

--- src/main/java/com/example/myworkschedule/servlets/BranchEmployeeServlet.java
+++ src/main/java/com/example/myworkschedule/servlets/BranchEmployeeServlet.java
@@ -1,7 +1,8 @@
 package com.example.myworkschedule.servlets;
 
 import com.example.myworkschedule.beans.User;
-import com.example.myworkschedule.dao.UserDao;
+import com.example.myworkschedule.dao.User.UserDaoImpl;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +15,7 @@ import java.util.List;
 public class BranchEmployeeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        UserDao dao = new UserDao();
+        UserDaoImpl dao = new UserDaoImpl();
         List<User> users = dao.searchBranchEmployee(1);
         req.setAttribute("users", users);
         req.getRequestDispatcher("/views/EmployerResult.jsp").forward(req, resp);
